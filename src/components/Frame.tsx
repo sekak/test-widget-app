a943e780506ee67d4236e80715394e9c79280563
sekak <ahmadesekak@gmail.com>
2024-08-04 17:40:57 +0100

    test1

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -1,15 +1,24 @@
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { motion } from 'framer-motion'
 import { CreateContext } from "../context/Context"
 import { buttonVr, variants } from "../motion"
 
 const Frame = () => {
     const { open, setOpen } = useContext(CreateContext)
-
+    let t = document.getElementById("iframe")?.getBoundingClientRect()?.top
+    console.log(document.getElementById("iframe")?.getBoundingClientRect().top)
+    useEffect(()=>{
+        let g = document.getElementById("img")
+        if(g)
+        {
+            console.log(t)
+            g.style.top = `${t && t + 15}px`
+        }
+    },[t])
     return (
         <motion.div className={`h-full w-full  `} >
-            <motion.iframe variants={variants} animate={open ? 'open' : 'closed'} src='https://widget-app-git-master-sekaks-projects.vercel.app/' className='w-[400px] h-[85%] rounded-[25px] fixed bottom-[120px] right-10 border-gray-100 shadowIframe mobileDv'></motion.iframe>
-            <motion.button variants={buttonVr} animate={open ? 'open' : 'closed'} className="absolute bg-red right-[15px] top-[22%] sm:hidden block" onClick={() => setOpen(!open)}>
+            <motion.iframe id="iframe" variants={variants} animate={open ? 'open' : 'closed'} src='https://widget-app-git-master-sekaks-projects.vercel.app/' className='w-[400px] h-[85%] rounded-[25px] fixed bottom-[120px] right-10 border-gray-100 shadowIframe mobileDv'></motion.iframe>
+            <motion.button variants={buttonVr} animate={open ? 'open' : 'closed'} id="img" className={`absolute bg-red right-[15px] sm:hidden block`} onClick={() => setOpen(!open)}>
                 <img src='./close.png' className='w-[20px] h-[20px]' />
             </motion.button>
         </motion.div>
