1e33171a32e6cfc8b4e9281c7f3e50d9c994eb83
sekak <ahmadesekak@gmail.com>
2024-08-05 13:37:16 +0100

    test4

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -1,28 +1,21 @@
 import { useContext, useEffect } from "react"
 import { motion } from 'framer-motion'
 import { CreateContext } from "../context/Context"
-import { buttonVr, variants } from "../motion"
+import {  variants } from "../motion"
 
 const Frame = () => {
     const { open, setOpen } = useContext(CreateContext)
-    let t = document.getElementById("iframe")?.getBoundingClientRect()?.top
-    console.log(document.getElementById("iframe")?.getBoundingClientRect().top)
-    useEffect(()=>{
-        let g = document.getElementById("img")
-        if(g)
-        {
-            console.log(t)
-            g.style.top = `${t && t + 15}px`
-        }
-    },[t])
+    
     return (
         <motion.div className={`h-full w-full  `} >
             <motion.iframe id="iframe" variants={variants} animate={open ? 'open' : 'closed'} src='https://widget-app-git-master-sekaks-projects.vercel.app/' className='w-[400px] h-[85%] rounded-[25px] fixed bottom-[120px] right-10 border-gray-100 shadowIframe mobileDv'></motion.iframe>
-            <motion.button variants={buttonVr} animate={open ? 'open' : 'closed'} id="img" className={`absolute bg-red right-[15px] sm:hidden block`} onClick={() => setOpen(!open)}>
-                <img src='./close.png' className='w-[20px] h-[20px]' />
-            </motion.button>
+            <div className="w-[400px] h-[85%] rounded-[25px] fixed bottom-[120px] right-10 border-gray-100 shadowIframe mobileDv">
+                {/* <div className="w-full absolute right-0"> */}
+                    <img alt='img1' src="./icon-1.png"  className='absolute right-0 top-5 w-[22px] h-[15px] '/>
+                {/* </div> */}
+            </div>
         </motion.div>
     )
 }
 
-export default Frame
+export default Frame  
--- src/motion.ts
+++ src/motion.ts
@@ -3,18 +3,6 @@ export const variants = {
     closed: { opacity: 0, y: "100px", transition: { duration: 0.5 }, },
 }
 
-export const buttonVr = {
-    open: {
-        opacity: 1,
-        y: 0,
-        transition: { duration: 0.5 }
-    },
-    closed: {
-        opacity: 0,
-        y: "100px",
-        transition: { duration: 0 }
-    }
-}
 
 export const variantsChange={
     open1: { opacity: 1,rotate:360,transition:{duration:0.8} },
