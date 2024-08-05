54aae12ea1114ce58deeb37e307542014e31d882
sekak <ahmadesekak@gmail.com>
2024-08-05 17:08:12 +0100

    test7

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -12,7 +12,7 @@ const Frame = () => {
                 id="iframe" 
                 variants={variants} 
                 animate={open ? 'open' : 'closed'} 
-                src='https://widget-app-bay.vercel.app/' 
+                src='https://widget-app-git-master-sekaks-projects.vercel.app/' 
                 className={`w-[400px] h-[85%] rounded-[25px] fixed bottom-[120px] right-10 border-gray-100 shadowIframe mobileDv z-[1]  ${!open && 'hidden'}`}
                 > 
                 </motion.iframe>
